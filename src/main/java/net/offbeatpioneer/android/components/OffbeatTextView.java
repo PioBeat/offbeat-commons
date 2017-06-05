9776c21ce105d39be3cd04eaf461048e5b2522a2
Dominik Grzelak <dominik@offbeat-pioneer.net>
2017-06-05 12:09:40 +0200

    typewriter animation for offbeattextview

--- src/main/java/net/offbeatpioneer/android/components/OffbeatTextView.java
+++ src/main/java/net/offbeatpioneer/android/components/OffbeatTextView.java
@@ -3,15 +3,25 @@ package net.offbeatpioneer.android.components;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Typeface;
+import android.os.Handler;
+import android.os.Message;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.widget.TextView;
 
 /**
- * Created by Dome on 06.10.2016.
+ * @author Dominik Grzelak
+ * @since 06.10.2016.
  */
-
 public class OffbeatTextView extends TextView {
+    private static final String TAG = OffbeatTextView.class.getSimpleName();
 
+    Handler completeCallbackHandler;
+    private Handler handler = new Handler();
+    private CharSequence text;
+    private int index;
+    private long delay = 250;
+    private boolean loopTyperwriter = false;
 
     public OffbeatTextView(Context context) {
         super(context);
@@ -35,8 +45,11 @@ public class OffbeatTextView extends TextView {
     private void init(AttributeSet attrs) {
         if (attrs != null) {
             TypedArray a = getContext().obtainStyledAttributes(attrs, R.styleable.OffbeatTextView);
-            String fontName = a.getString(R.styleable.OffbeatTextView_font);
 
+            this.delay = a.getInteger(R.styleable.OffbeatTextView_characterDelay, (int) this.delay);
+            this.loopTyperwriter = a.getBoolean(R.styleable.OffbeatTextView_loopTypewriter, this.loopTyperwriter);
+
+            String fontName = a.getString(R.styleable.OffbeatTextView_font);
             try {
                 if (fontName != null) {
                     Typeface myTypeface = Typeface.createFromAsset(getContext().getAssets(), "fonts/" + fontName);
@@ -48,6 +61,72 @@ public class OffbeatTextView extends TextView {
 
             a.recycle();
         }
+    }
+
+    private Runnable characterAdder = new Runnable() {
+
+        @Override
+        public void run() {
+            setText(text.subSequence(0, index++));
+            if (index <= text.length()) {
+                handler.postDelayed(characterAdder, delay);
+            } else {
+                if (null != completeCallbackHandler) {
+                    completeCallbackHandler.sendMessage(new Message());
+                    if (loopTyperwriter) {
+                        index = 0;
+                        animateText();
+                    }
+                } else
+                    Log.d(TAG, "Complete callback handler not set. Cannot send message.");
+            }
+        }
+    };
+
+    public void animateText() {
+        this.animateText(getText());
+    }
+
+    public void animateText(CharSequence txt) {
+        text = txt;
+        index = 0;
+
+        setText("");
+        handler.removeCallbacks(characterAdder);
+        handler.postDelayed(characterAdder, delay);
+    }
+
+    /**
+     * Delay in milliseconds
+     *
+     * @param delay milliseconds
+     */
+    public void setCharacterDelay(long delay) {
+        this.delay = delay;
+    }
+
+    public Handler getCompleteCallbackHandler() {
+        return completeCallbackHandler;
+    }
+
+    public void setCompleteCallbackHandler(Handler completeCallbackHandler) {
+        this.completeCallbackHandler = completeCallbackHandler;
+    }
+
+    public long getDelay() {
+        return delay;
+    }
+
+    public void setDelay(long delay) {
+        this.delay = delay;
+    }
+
+    public boolean isLoopTyperwriter() {
+        return loopTyperwriter;
+    }
+
+    public void setLoopTyperwriter(boolean loopTyperwriter) {
+        this.loopTyperwriter = loopTyperwriter;
     }
 
     @Override
