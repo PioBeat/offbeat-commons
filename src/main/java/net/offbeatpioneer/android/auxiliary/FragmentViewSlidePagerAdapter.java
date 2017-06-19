34296def380136fb7f77ba028af729e308c47b0d
Dominik Grzelak <dominik@offbeat-pioneer.net>
2017-06-19 19:42:48 +0200

    FragmentViewSlidePagerAdapter has new hool method createItem(int), which is called when registeredFragments size is zero (adding fragments before not necessary)

--- src/main/java/net/offbeatpioneer/android/auxiliary/FragmentViewSlidePagerAdapter.java
+++ src/main/java/net/offbeatpioneer/android/auxiliary/FragmentViewSlidePagerAdapter.java
@@ -3,66 +3,97 @@ package net.offbeatpioneer.android.auxiliary;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
-import android.util.SparseArray;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
- * A simple pager adapter that represents Fragment objects, in sequence.
+ * A simple and convenient to use pager adapter that represents Fragment objects, in sequence.
+ * <p>
+ * In this basic implementation of {@link FragmentStatePagerAdapter} you have to instantiate
+ * your {@link Fragment}s first and add it with {@code FragmentViewSlidePagerAdapter#addFragmet}.
+ * <p>
+ * Another possibility is to extend this class and overwrite the {@link FragmentViewSlidePagerAdapter#createItem(int)}
+ * method to instantiate the Fragment at the moment when {@link FragmentStatePagerAdapter#getItem(int)}
+ * is normally called. {@link FragmentViewSlidePagerAdapter#createItem(int)} is a hook method which
+ * is called inside {@link FragmentViewSlidePagerAdapter#getItem(int)} when {@code registeredFragments}
+ * is {@code null}.
  *
  * @author Dominik Grzelak
+ * @see <a href="https://developer.android.com/reference/android/support/v4/app/FragmentStatePagerAdapter.html">FragmentStatePagerAdapter</a>
  */
 public class FragmentViewSlidePagerAdapter extends FragmentStatePagerAdapter {
-    private SparseArray<Fragment> registeredFragments = new SparseArray<>();
-    //    private List<Fragment> fragmentList = new ArrayList<>();
-    private int numPages = -1;
+    private List<Fragment> registeredFragments = new ArrayList<>();
+    private int numPages = 0;
 
     public FragmentViewSlidePagerAdapter(FragmentManager fm) {
         super(fm);
     }
 
 
+    /**
+     * Get the fragment at the specified {@code position}.
+     * <p>
+     * If {@code registeredFragments} is null or empty the method
+     * {@link FragmentViewSlidePagerAdapter#createItem(int)} is called to instantiate a fragment.
+     * You have to override this convenient method to use it here. The
+     * default return value is {@code null}.
+     *
+     * @param position position of the fragment in {@code registeredFragments}
+     * @return the fragment, or {@code null}
+     */
     @Override
     public Fragment getItem(int position) {
-//        Fragment frag = null;
-//        switch (position) {
-//            case 0:
-//                frag = IntroAddServer.newInstance(null);
-//                break;
-//            case 1:
-//                frag = IntroAddServer.newInstance("page2");
-//                break;
-//        }
-//        registeredFragments.put(position, frag);
-
+        if (registeredFragments == null || registeredFragments.size() == 0) {
+            return createItem(position);
+        }
         return registeredFragments.get(position);
+    }
+
+    /**
+     * Instantiates a fragment at the specified position.
+     * The default implementation is empty and returning {@code null}.
+     * Override this method to implement your own logic for creating different {@link Fragment}s.
+     *
+     * @param position position of the fragment
+     * @return the {@link Fragment} or {@code null}
+     */
+    public Fragment createItem(int position) {
+        return null;
     }
 
     public Fragment getRegisteredFragment(int position) {
         return registeredFragments.get(position);
     }
 
+    /**
+     * Adds a fragment to {@code registeredFragments} at the end of the list.
+     *
+     * @param fragment fragment to add
+     */
     public void addFragmet(Fragment fragment) {
-//        fragmentList.addAll(Arrays.asList(fragments));
-        registeredFragments.put(registeredFragments.size(), fragment);
+        registeredFragments.add(fragment);
     }
-
-//        @Override
-//        public Object instantiateItem(ViewGroup container, int position) {
-//            Fragment fragment = (Fragment) super.instantiateItem(container, position);
-//            registeredFragments.put(position, fragment);
-//            return fragment;
-//        }
 
     public int getNumPages() {
         return numPages;
     }
 
+    /**
+     * Set a different page count e.g. to limit the accessible fragments.
+     * Value should be negative to use the size of {@code registeredFragments}, or
+     * positive to use the specified count.
+     *
+     * @param numPages number of pages to display, -1 if the real registered fragments size should
+     *                 be used
+     */
     public void setNumPages(int numPages) {
         this.numPages = numPages;
     }
 
     @Override
     public int getCount() {
-        if (numPages == -1) {
+        if (numPages < 0) {
             return registeredFragments.size();
         }
         return numPages;
